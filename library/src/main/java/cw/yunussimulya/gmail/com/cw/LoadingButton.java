15f73ede77fbdac5d6633d42026ef2bc50039617
yunussim <yunussimulya@gmail.com>
2018-11-15 11:57:39 +0700

    Add widget - Loading Button

---
+++ library/src/main/java/cw/yunussimulya/gmail/com/cw/LoadingButton.java
@@ -0,0 +1,80 @@
+package cw.yunussimulya.gmail.com.cw;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+public class LoadingButton extends RelativeLayout {
+
+    AppCompatTextView tvText;
+    ProgressBar pbLoading;
+
+    public LoadingButton(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public LoadingButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public LoadingButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        inflate(getContext(), R.layout.button_loading, this);
+        pbLoading = findViewById(R.id.pbLoading);
+        tvText = findViewById(R.id.tvText);
+        String text = "";
+        int textColor = Color.BLACK;
+        float textSize = 14;
+        int loadingColor = Color.WHITE;
+        if (attrs != null) {
+            TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.LoadingButton, 0, 0);
+            try {
+                text = ta.getString(R.styleable.LoadingButton_lbText);
+                textColor = ta.getColor(R.styleable.LoadingButton_lbTextColor, Color.BLACK);
+                textSize = ta.getDimensionPixelSize(R.styleable.LoadingButton_lbTextSize, 14);
+                loadingColor = ta.getColor(R.styleable.LoadingButton_lbLoadingColor, Color.WHITE);
+            } finally {
+                ta.recycle();
+            }
+        }
+        tvText.setText(text);
+        tvText.setTextColor(textColor);
+        Log.e("textSize", textSize + "");
+        tvText.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        pbLoading.getIndeterminateDrawable().setColorFilter(loadingColor, PorterDuff.Mode.MULTIPLY);
+        stopLoading();
+    }
+
+    public void setText(@NonNull String text) {
+        tvText.setText(text);
+    }
+
+    public String getText() {
+        return tvText.getText().toString();
+    }
+
+    public void startLoading() {
+        tvText.setVisibility(View.GONE);
+        pbLoading.setVisibility(View.VISIBLE);
+    }
+
+    public void stopLoading() {
+        tvText.setVisibility(View.VISIBLE);
+        pbLoading.setVisibility(View.GONE);
+    }
+}

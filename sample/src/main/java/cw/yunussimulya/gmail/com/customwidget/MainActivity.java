89b336f083876327580fbf7f3847c562fdcd91db
yunussim <yunussimulya@gmail.com>
2018-11-15 12:17:15 +0700

    Add Sample - Title Text View

--- sample/src/main/java/cw/yunussimulya/gmail/com/customwidget/MainActivity.java
+++ sample/src/main/java/cw/yunussimulya/gmail/com/customwidget/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import cw.yunussimulya.gmail.com.cw.LoadingButton;
+import cw.yunussimulya.gmail.com.cw.TitleTextView;
 
 public class MainActivity extends AppCompatActivity {
 
     LoadingButton loadingButton;
+    TitleTextView ttv;
+    Button bClearTTV, bFillTTV, bErrorTTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
                         loadingButton.stopLoading();
                     }
                 }, 1000);
+            }
+        });
+
+        ttv = findViewById(R.id.ttv);
+
+        bClearTTV = findViewById(R.id.bClearTTV);
+        bFillTTV = findViewById(R.id.bFillTTV);
+        bErrorTTV = findViewById(R.id.bErrorTTV);
+        bClearTTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ttv.setContent("");
+            }
+        });
+
+        bFillTTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ttv.setContent("Content is here");
+            }
+        });
+
+        bErrorTTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ttv.showError(!ttv.isError());
             }
         });
     }
